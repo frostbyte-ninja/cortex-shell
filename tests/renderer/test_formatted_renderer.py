fdb1f53e800ee916d0a45b16f4366a861f27444d
frostbyte-ninja <117345621+frostbyte-ninja@users.noreply.github.com>
2024-01-07 12:46:33 +0100

    test: add renderer tests

---
+++ tests/renderer/test_formatted_renderer.py
@@ -0,0 +1,35 @@
+import pytest
+
+from cortex_shell import constants as C  # noqa: N812
+from cortex_shell.renderer.formatted_renderer import FormattedRenderer
+
+
+@pytest.fixture()
+def formatted_renderer(mock_role):
+    return FormattedRenderer(mock_role)
+
+
+class TestFormattedRenderer:
+    def test_enter(self, mocker, formatted_renderer):
+        mock = mocker.patch(f"{C.PROJECT_MODULE}.renderer.formatted_renderer.Live")
+
+        with formatted_renderer:
+            mock.return_value.__enter__.assert_called_once()
+
+    def test_exit(self, mocker, formatted_renderer):
+        mock = mocker.patch(f"{C.PROJECT_MODULE}.renderer.formatted_renderer.Live")
+
+        with formatted_renderer:
+            pass
+
+        mock.return_value.__exit__.assert_called_once()
+
+    def test_call(self, mocker, formatted_renderer):
+        live_mock = mocker.patch(f"{C.PROJECT_MODULE}.renderer.formatted_renderer.Live")
+        markdown_mock = mocker.patch(f"{C.PROJECT_MODULE}.renderer.formatted_renderer.Markdown")
+
+        with formatted_renderer:
+            formatted_renderer("text1", "text2")
+
+        live_mock.return_value.update.assert_called_once()
+        markdown_mock.assert_called_with("text1text2", code_theme=mocker.ANY, style=mocker.ANY)
---
+++ tests/renderer/test_plain_renderer.py
@@ -0,0 +1,35 @@
+import pytest
+
+from cortex_shell import constants as C  # noqa: N812
+from cortex_shell.renderer.plain_renderer import PlainRenderer
+
+
+@pytest.fixture()
+def plain_renderer(mock_role):
+    return PlainRenderer(mock_role)
+
+
+class TestPlainRenderer:
+    def test_enter(self, mocker, plain_renderer):
+        mock = mocker.patch(f"{C.PROJECT_MODULE}.renderer.plain_renderer.print_formatted_text")
+
+        with plain_renderer:
+            mock.assert_not_called()
+
+    def test_exit(self, mocker, plain_renderer):
+        mock = mocker.patch(f"{C.PROJECT_MODULE}.renderer.plain_renderer.print_formatted_text")
+
+        with plain_renderer:
+            pass
+
+        mock.assert_called_once()
+
+    def test_call(self, mocker, plain_renderer):
+        print_formatted_text_mock = mocker.patch(f"{C.PROJECT_MODULE}.renderer.plain_renderer.print_formatted_text")
+        colored_text_mock = mocker.patch(f"{C.PROJECT_MODULE}.renderer.plain_renderer.get_colored_text")
+
+        with plain_renderer:
+            plain_renderer("text1", "text2")
+
+        print_formatted_text_mock.assert_any_call(mocker.ANY, end="")
+        colored_text_mock.assert_called_with("text2", mocker.ANY)
