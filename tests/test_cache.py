9e43fab3f674ca6df30b7cfc4549cbca46ebf5fe
frostbyte-ninja <117345621+frostbyte-ninja@users.noreply.github.com>
2024-01-12 09:46:16 +0100

    test: test cache directory creation

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import pytest
 
 from cortex_shell.cache import Cache
 
@@ -7,11 +7,22 @@ def drain_generator(generator):
     return "".join(generator)
 
 
-class TestCache:
-    def test_with_cache(self, mocker, tmp_dir_factory):
-        cache_path = Path(tmp_dir_factory.get())
-        cache = Cache(5, cache_path)
+@pytest.fixture()
+def cache_path(tmp_dir_factory):
+    temp_dir = tmp_dir_factory.get() / "some_dir"
+    assert not temp_dir.exists()
+    return temp_dir
 
+
+@pytest.fixture()
+def cache(cache_path):
+    cache = Cache(5, cache_path)
+    assert cache_path.exists()
+    return cache
+
+
+class TestCache:
+    def test_with_cache(self, mocker, cache, cache_path):
         mock_function = mocker.MagicMock(return_value="result")
         function = cache(mock_function)
 
@@ -25,10 +36,7 @@ class TestCache:
         assert mock_function.call_count == 1
         assert result == "result"
 
-    def test_without_cache(self, mocker, tmp_dir_factory):
-        cache_path = Path(tmp_dir_factory.get())
-        cache = Cache(5, cache_path)
-
+    def test_without_cache(self, mocker, cache, cache_path):
         mock_function = mocker.MagicMock(return_value="result")
         function = cache(mock_function)
 
@@ -41,10 +49,7 @@ class TestCache:
         assert mock_function.call_count == 3
         assert result == "result"
 
-    def test_multiple_requests(self, mocker, tmp_dir_factory):
-        cache_path = Path(tmp_dir_factory.get())
-        cache = Cache(10, cache_path)
-
+    def test_multiple_requests(self, cache, cache_path):
         @cache
         def function(*_args, **kwargs):
             return ""
@@ -58,10 +63,7 @@ class TestCache:
         cached_files = list(cache_path.glob("*"))
         assert len(cached_files) == 5
 
-    def test_cache_limit(self, tmp_dir_factory):
-        cache_path = Path(tmp_dir_factory.get())
-        cache = Cache(2, cache_path)
-
+    def test_cache_limit(self, cache, cache_path):
         @cache
         def function(*_args, **kwargs):
             return ""
@@ -69,6 +71,9 @@ class TestCache:
         drain_generator(function(caching=True, messages=[]))
         drain_generator(function(caching=True, param1=None, messages=[]))
         drain_generator(function(caching=True, param2=None, messages=[]))
+        drain_generator(function(caching=True, param3=None, messages=[]))
+        drain_generator(function(caching=True, param4=None, messages=[]))
+        drain_generator(function(caching=True, param5=None, messages=[]))
 
         cached_files = list(cache_path.glob("*"))
-        assert len(cached_files) == 2
+        assert len(cached_files) == 5
